daac354c6a018615923e948e8818b0305e881b94
Documenter.jl <documenter@juliadocs.github.io>
2022-04-22 15:17:54 +0000

    build based on 12a6444

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ControlArduino","category":"page"},{"location":"#ControlArduino","page":"Home","title":"ControlArduino","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ControlArduino.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ControlArduino]","category":"page"}]
+}
