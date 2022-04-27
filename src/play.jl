5b055f6f93382a950b15aff9842aca9c404251a9
DarioSarra <dariosarra@gmail.com>
2022-04-27 14:19:37 +0100

    restart from skeleton

--- src/play.jl
+++ src/play.jl
@@ -1,25 +1,67 @@
 using Revise, Distributed
 # this allows to create separate process so that you can run the opto on process 2 while keep using Julia pn process 1
- # add a process if there are less than 2
-nprocs() != 3 && addprocs(2; exeflags="--project")
-@everywhere using Pkg   # required
-@everywhere Pkg.activate(".")
-@everywhere using ControlArduino
+# add a process if there are less than 2
+using ControlArduino
+# nprocs() != 3 && addprocs(2; exeflags="--project")
+# @everywhere using Pkg   # required
+# @everywhere Pkg.activate(".")
+# @everywhere using ControlArduino
 ##
-test1 = SessionStruct("test", 54.0)
+using LibSerialPort
+ST = SessionStruct("test", 54.0)
+port = SerialPort(Arduino_dict[ST.Arduino])
+if !port.is_open
+      open(port)
+      set_speed(port,115200)
+      println("Opening Port")
+end
+close(port)
+is_running = true
+@async begin
+    while is_running
+        if bytesavailable(port) > 0
+            m = readline(port)
+            println(m)
+        end
+    end
+end
+
+port.is_open
+set_read_timeout(port, 2)
+try
+    line1 = readuntil(sp, '\n')
+    line2 = readuntil(sp, '\n')
+catch e
+    if isa(e, LibSerialPort.Timeout)
+        println("Too late!")
+    else
+        rethrow()
+    end
+end
+m = readuntil(port, '\r')
+close(port)
+##
+ST = SessionStruct("test", 54.0)
+running!(test1.Arduino , true)
+task = @spawnat 2 run_task(test1)
+running!(test1.Arduino , false)
+##
+port = SerialPort(Arduino_dict[ST.Arduino])
+port.is_open
+open(port)
+close(port)
+@spawnat 2 close(port)
+ArduinosController[ST.Arduino]
+@fetchfrom 2 ArduinosController[ST.Arduino]
 running(1)
 ##
-replace(string(today()), "-"=>"")
-
-
-list_ports()
-ports = get_port_list()
-
-sp = LibSerialPort.open("COM4", 115200)
-set_read_timeout(sp, 5)
-bytesavailable(sp) > 0 && readline(sp)
-write(sp, 'S')
-
-
-close(sp)
-sp_flush(sp,SP_BUF_INPUT)
+using LibSerialPort
+port = SerialPort(Arduino_dict[ST.Arduino])
+file = ST.FileName
+if !port.is_open
+      open(port)
+      set_speed(port,115200)
+      println("Opening Port")
+  end
+ close(port)
+##
