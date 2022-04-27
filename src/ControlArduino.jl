8a97143a449d4083592af8b3db4ab06b4a6ef778
DarioSarra <dariosarra@gmail.com>
2022-04-27 12:16:18 +0100

    dependecies issues

--- src/ControlArduino.jl
+++ src/ControlArduino.jl
@@ -2,7 +2,7 @@ module ControlArduino
 
 # Write your package code here.
 using Reexport
-@reexport using Revise, DataFrames, LibSerialPort, Interact, Blink, CSSUtil, Distributed
+@reexport using  LibSerialPort, Interact, Blink, CSSUtil, Distributed
 @reexport import Dates.today
 
 include("StimulationStructure.jl")
