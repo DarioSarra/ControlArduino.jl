7fb49e45998e2d477bbbcf97e4cbab5f585f11fb
DarioSarra <dariosarra@gmail.com>
2022-05-05 16:34:54 +0100

    minor

--- src/play.jl
+++ src/play.jl
@@ -19,35 +19,7 @@ es.Session = SessionStruct("test",24,"COM4")
 ##
 w_ses = widget(s); w = Window(); body!(w,w_ses)
 w_freq = widget(f); w = Window(); body!(w,w_freq)
-
+##
+w_freq[]
 w_ses[]
 ##
-FreqStruct()
-c = stim_wid()
-c[]
-c2 = widget(f);
-c2[]
-##
-function Widgets.widget(f::FreqStruct)
-	frequencies = Observable{FreqStruct}(f)
-	freq_spins = Observable{Any}(dom"div"())
-	freq_n = labeled_widget("Select # of stim protocols between 1 and 10",spinbox;val = (1:10), value = 1)
-	map!(t-> dom"div"(stim_wid(t)),freq_spins,freq_n)
-	d = OrderedDict(:n => freq_spins, :frequencies => freq_spins)
-	o = Observable{FreqStruct}(f)
-	Interact.@map! o begin
-		FreqStruct(&freq_spins)
-	end
-	w = Interact.Widget{:Stim_sel}(d, output = o)
-	@layout! w vbox(:n,:frequencies)
-	return w
-end
-
-
-##
-chose_stims()
-f
-t = widget(f)
-##
-c = [spinbox() for x in 1:4]
-vbox(c)
