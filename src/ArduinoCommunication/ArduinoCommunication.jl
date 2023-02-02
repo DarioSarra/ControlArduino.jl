cffa23f669b2cdd5493ccf43fa76d2f93f6638c3
DarioSarra <dariosarra@gmail.com>
2023-02-02 14:54:25 +0000

    Rearranged files location and names

--- src/ArduinoCommunication/ArduinoCommunication.jl
+++ src/ArduinoCommunication/ArduinoCommunication.jl
@@ -1,10 +1,11 @@
-# Arduino_dict = Dict(1 => "/dev/ttyACM1",
-#     2 => "/dev/ttyACM2",
-#     3 => "/dev/ttyACM0",
-#     4 =>"COM4", 5 => "COM5")
-const ArduinosController = falses(8)
 ports_available = get_port_list()
+
+#create a vector of booleans for each process, used to open and close communications with serial ports. Warnig max is 8
+const ArduinosController = falses(length(ports_available))
+
+#create a dictionary to bind each serial port to an index on the ArduinoController vector
 const Arduino_dict = Dict(k => v for (k,v) in zip(ports_available,1:length(ports_available)))
+
 #function take the runing state of the Arduino from process 2
 running(Arduino_port) = @fetchfrom 1 ArduinosController[get(Arduino_dict,Arduino_port,0)]
 #function change the runing state of the Arduino from process 2
--- src/play.jl
+++ src/play.jl
@@ -1,30 +1,34 @@
 using Revise, Interact, Blink, CSSUtil, LibSerialPort
-using  Distributed
-
-# to control maintain an oper communication Arduino without freezing the current terminal we need additional processes
-nprocs() != 3 && addprocs(2,exeflags="--project")
+using Distributed
+##
+# to control maintain an open communication Arduino without freezing the current terminal we need additional processes
+nprocs() != 3 && addprocs(2, exeflags="--project")
 workers()
 #= every process has an independet library upload. We can use @everywhere to load libraries in all processes
 the first step is always to activate the PKG library on the new processes, or we can't upload anything else=#
-@everywhere using Pkg   
+@everywhere using Pkg
 @everywhere Pkg.activate(".")
-# The following libraries and functions need to work on the other processes, in parallel with the main one
+# The following basic libraries need to work on all processes
 @everywhere using LibSerialPort
 @everywhere import Dates.today, Dates.Date
-@everywhere include("ArduinoCommunication.jl")
-@everywhere include("StimulationStructure.jl")
+
+# The following files contain functions needed in all processes. Files have to be loaded in order: Structures followed by ArduinoCommunication
+@everywhere include(joinpath("DataStructures","Structures.jl"))
+@everywhere include(joinpath("ArduinoCommunication","ArduinoCommunication.jl"))
+
 # the following functions and values define the GUI and are only loaded in the main process 
-include("Premade_Stim_Protocols.jl")
-include("GUI_elements.jl")
+include(joinpath("GUI", "Premade_Stim_Protocols.jl"))
+include(joinpath("GUI", "GUI_elements.jl"))
 ##
 #= A gui is build combining multiple structures. These structures distinguished by the type of info about 
-the experiment that they define.=# 
+the experiment that they define.=#
 # The Frequencies structure defines 
 f = FreqStruct(s1)
 #The session structure has info about the day, subject name and it combinesthem to define the output file location
 s = SessionStruct()
-es = ExpStruct(f,s,60,30)
-w_ex = widget(es); w = Window(); body!(w,fetch(w_ex))
+es = ExpStruct(f, s, 60, 30)
+w_ex = widget(es);
+w = Window();body!(w, fetch(w_ex));
 ##
 ex = w_ex[]
 Ard = ex.Session.Arduino
@@ -34,43 +38,6 @@ stimulations = ex.Frequencies.Stimulations
 stimfreq1 = rm_missing(ex.Frequencies.Frequency1)
 stimdur1 = rm_missing(ex.Frequencies.Volumes1)
 stimfreq2 = rm_missing(ex.Frequencies.Frequency2)
-stimdur2= rm_missing(ex.Frequencies.Volumes2)
+stimdur2 = rm_missing(ex.Frequencies.Volumes2)
 filename = ex.Session.FileName
 ##
-running!(Ard,true)
-task = @spawnat :any run_opto(Ard,
-    stimvolumes, unstimvolumes, stimulations,
-    stimfreq1,stimdur1,
-    stimfreq2,stimdur2,
-    filename)
-running!(Ard,false)
-fetch(task)
-maximum(skipmissing(stimfreq1))
-##
-ex = w_ex[]
-running!(ex.Session.Arduino,true)
-task = @spawnat :any run_opto(ex)
-fetch(task)
-running!(ex.Session.Arduino,false)
-##
-Ard  = "COM4"
-Arduino_dict[Ard]
-stimvolumes = 20
-unstimvolumes = 5
-stimulations = 6
-stimfreq1 = [12,0,4,12,0,4]
-stimdur1 = repeat([5],6)
-stimfreq2 = [0,4,12,4,12,0]
-stimdur2 = repeat([5],6)
-filename = "C:\\Users\\precl\\OneDrive\\Documents\\ArduinoData\\test.csv"
-
-##
-running!(Ard,true)
-task = @spawnat :any run_opto(Ard,
-    stimvolumes, unstimvolumes, stimulations,
-    stimfreq1,stimdur1,
-    stimfreq2,stimdur2,
-    filename)
-##
-running!(Ard,false)
-fetch(task)
