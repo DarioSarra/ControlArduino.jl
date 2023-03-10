c00e6f094b68edd82d2c3ece9f25cc432c23952d
DarioSarra <dariosarra@gmail.com>
2023-03-10 14:57:51 +0000

    new frequencies

--- src/GUI/Premade_Stim_Protocols.jl
+++ src/GUI/Premade_Stim_Protocols.jl
@@ -1,3 +1,4 @@
+# set of premade frequencies. Tuples order means: Hz_1, mS_1, Hz_2, mS_2, Hz_LED
 low = (4,1000,0,0,4)
 medium = (8,500,0,500,8)
 high = (16,333,0,667,16)
@@ -5,3 +6,17 @@ s1 = [high,low,medium,low,high,medium]
 s2 = [high,low,medium,low,medium,high]
 s3 = [low,high,medium,high,low,medium]
 s4 = [medium,high,low,high,medium,low]
+
+sust4 = (4,1000,0,0,4)
+sust6 = (6,1000,0,0,6)
+sust8 = (8,1000,0,0,8)
+sust10 = (10,1000,0,0,10)
+sust12 = (12,1000,0,0,12)
+sust14 = (14,1000,0,0,14)
+
+trans4 = (4,1000,0,0,4)
+trans6 = (6,667,0,333,6)
+trans8 = (8,500,0,500,8)
+trans10 = (10,400,0,600,10)
+trans12 = (12,334,0,666,12)
+trans14 = (14,286,0,714,14)
