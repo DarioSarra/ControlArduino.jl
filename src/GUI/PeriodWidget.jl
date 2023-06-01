5fbe405a74b0ff9aed0e4c0a1a146a759b76de38
DarioSarra <dariosarra@gmail.com>
2023-06-01 12:11:58 +0100

    simplify the folder

--- src/GUI/PeriodWidget.jl
+++ src/GUI/PeriodWidget.jl
@@ -8,8 +8,8 @@ function Widgets.widget(p::PeriodStruct)
     pre_stim = labeled_widget("Pre-stimulation period",spinbox,value = pre_s)
 	in_stim = labeled_widget("Stimulation period",spinbox,value = in_s)
 	post_stim = labeled_widget("Post-stimulation period",spinbox,value = post_s)
-	stim_vol = labeled_widget("Volumes with laser ON in stimulation period",spinbox,value = sv)
-	unstim_vol = labeled_widget("Volumes with laser OFF in stimulation period",spinbox,value = uv)
+	stim_vol = labeled_widget("Repetitions with laser ON in stimulation period",spinbox,value = sv)
+	unstim_vol = labeled_widget("Repetitions with laser OFF in stimulation period",spinbox,value = uv)
     ## we need to create an observable variable to edit it with the gui, so we create an observable version of the initial PeriodStruct p 
 	o = Observable{PeriodStruct}(p)
 	coll = button("Prepare Periods")
@@ -30,11 +30,11 @@ function Widgets.widget(p::PeriodStruct)
                     ),
             output = o) #the widget needs to map it's output to the observable
     @layout! wdg vbox(
-        "Define overall run structure, with # volumes per period (before, during, and after stimulation)",
+        "Define overall run structure, with # repetitions per period (before, during, and after stimulation)",
         vskip(0.5em),
         hbox(:PreStim,hskip(1em), :InStim, hskip(1em), :PostStim),
         vskip(1em),
-        "Define number of volumes to alternate laser on-off during the stimulation period",
+        "Define number of repetitions to alternate laser on-off during the stimulation period",
         vskip(0.5em),
         hbox(:StimON,hskip(1em),:StimOFF),
         vskip(0.1em),
