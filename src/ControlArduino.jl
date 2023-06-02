1e4c0168e852763b141f8fff66ef1a7647ef0f7c
DarioSarra <dariosarra@gmail.com>
2023-06-02 13:20:58 +0100

    module attempt

--- src/ControlArduino.jl
+++ src/ControlArduino.jl
@@ -1,20 +1,44 @@
 module ControlArduino
-    using Distributed
-    nprocs() != 3 && addprocs(2,exeflags="--project")
-    workers()
-    @everywhere using Pkg   # required
-    @everywhere Pkg.activate(".")
-    @everywhere using LibSerialPort, Distributed
-    @everywhere import Dates.today, Dates.Date
+using Interact, Blink, CSSUtil, LibSerialPort, Distributed
+# to control maintain an open communication Arduino without freezing the current terminal we need additional processes
+nprocs() != 3 && addprocs(2, exeflags="--project")
+workers()
+#= every process has an independet library upload. We can use @everywhere to load libraries in all processes
+the first step is always to activate the PKG library on the new processes, or we can't upload anything else=#
+@everywhere using Pkg
+@everywhere Pkg.activate(".") ##activate control arduino environment in all workers
+# The following basic libraries need to work on all processes
+@everywhere using LibSerialPort, Distributed
+@everywhere import Dates.today, Dates.Date
 
-    using Interact
-    include("StimulationStructure.jl")
-    const ArduinosController = falses(8)
-    ports_available = get_port_list()
-    const Arduino_dict = Dict(k => v for (k,v) in zip(ports_available,1:length(ports_available)))
-    @everywhere include(joinpath(@__DIR__,"RunStim.jl"))
-    include(joinpath(@__DIR__,"ExpWidget.jl"))
+# The following files contain functions needed in all processes. Files have to be loaded in order: Structures followed by ArduinoCommunication
+@everywhere include(joinpath("DataStructures","SessionFile_structure.jl"))
+@everywhere include(joinpath("DataStructures","Frequencies_structure.jl"))
+@everywhere include(joinpath("DataStructures","Periods_structure.jl"))
+@everywhere include(joinpath("DataStructures","Experiment_structure.jl"))
+@everywhere include(joinpath("ArduinoCommunication","SerialPortsManager.jl"))
+@everywhere include(joinpath("ArduinoCommunication","MessageEncoding.jl"))
+@everywhere include(joinpath("ArduinoCommunication","ArduinoCommunication.jl"))
 
-    export SessionStruct, FreqStruct, ExpStruct
-    export run_task, ArduinosController, Arduino_dict, running, running!
+# the following functions and values define the GUI and are only loaded in the main process 
+include(joinpath("GUI", "Premade_Stim_Protocols.jl"))
+include(joinpath("GUI", "GUI_utilities.jl"))
+include(joinpath("GUI", "SessionWidget.jl"))
+include(joinpath("GUI", "FrequencyWidget.jl"))
+include(joinpath("GUI", "PeriodWidget.jl"))
+include(joinpath("GUI", "ExperimentWidget.jl"))
+
+function laser_gui()
+    f = FreqStruct();
+    s = SessionStruct();
+    p = PeriodStruct(60,600,60,10, 50);
+    es = ExpStruct(s,p,f);
+    w_ex = widget(es);
+    w = Window();body!(w, fetch(w_ex));
+    return es
+end
+
+export SessionStruct, FreqStruct, ExpStruct
+export laser_gui
+
 end
